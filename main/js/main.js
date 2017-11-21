a3608e4611acdf7f94fe9adc22ea3298216a1760
Amidou Kante <kamdiou@gmail.com>
2017-11-20 23:19:18 -0500

    Removed used files

--- main/js/main.js
+++ main/js/main.js
@@ -9,28 +9,49 @@ window.onload=()=>{
     var imageIsLoaded = undefined;
     let scrollAmount = window.pageYOffset;
     let slothDiv = document.getElementById('islands');
+    let mountainsDiv = document.getElementById('mountains');
+    let jungleDiv = document.getElementById('jungle');
+    let desertsDiv = document.getElementById('deserts');
+    
+    if(slothDiv.offsetTop - window.innerHeight < window.pageYOffset){
+        fetchImages();
+    }
 
     window.onscroll = (ev)=>{
-        scrollAmount = window.pageYOffset;
+        /* Checks to see if the gallery is in view by comparing it's position
+        with the height and YOffset of the window.  */
         if(slothDiv.offsetTop - window.innerHeight < window.pageYOffset){
-            fetchImages()
+            fetchImages();
+        }else{
+            null;
         }
     }
 
     function fetchImages(){
         if(imageIsLoaded !== undefined){
             let slothImage = sessionStorage.getItem("sloth-image");
+            let mountainsImage = sessionStorage.getItem("mountains-image");
+            let jungleImage = sessionStorage.getItem("jungle-image");
+            let desertsImage = sessionStorage.getItem("deserts-image");
+
             slothDiv.style.backgroundImage = `url(${slothImage})`;
+            mountainsDiv.style.backgroundImage = `url(${mountainsImage})`;
+            jungleDiv.style.backgroundImage = `url(${jungleImage})`;
+            desertsDiv.style.backgroundImage = `url(${desertsImage})`;
+
         }else{
             console.log('fetching image')
-            fetch("images/sloth.jpg", myInit)
+            fetch("images/", myInit)
             .then(res=>{
-                sessionStorage.setItem("sloth-image", res.url)
+                sessionStorage.setItem("sloth-image", `${res.url}sloth.jpg`)
+                sessionStorage.setItem("mountains-image", `${res.url}mountains.jpg`)
+                sessionStorage.setItem("jungle-image", `${res.url}jaguar.jpg`)
+                sessionStorage.setItem("deserts-image", `${res.url}fox.png`)
                 imageIsLoaded = true;
                 fetchImages();
             })
             .catch(err=>{
-                
+                console.log(err)
             })
         }
     }
