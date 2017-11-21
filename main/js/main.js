7e26c193e88d593444395da0d77b40a8a2bb8762
Amidou Kante <kamdiou@gmail.com>
2017-11-21 01:18:38 -0500

    fixed problem of re fetching

--- main/js/main.js
+++ main/js/main.js
@@ -6,33 +6,46 @@ window.onload=()=>{
         cache: 'default' 
     }
 
-    var imageIsLoaded = undefined;
     let slothDiv = document.getElementById('islands');
     let mountainsDiv = document.getElementById('mountains');
     let jungleDiv = document.getElementById('jungle');
     let desertsDiv = document.getElementById('deserts');
 
     /* If a user refeshes or revist the page this will load already stored images */
-    if(slothDiv.offsetTop - window.innerHeight < window.pageYOffset && sessionStorage.imageIsLoaded){
-        fetchImages();
-    }
-
-    window.onscroll = (ev)=>{
-        /* Checks to see if the gallery is in view by comparing it's position
-        with the height and YOffset of the window.  */
-        if(slothDiv.offsetTop - window.innerHeight < window.pageYOffset){
+    if((slothDiv.offsetTop - window.innerHeight < window.pageYOffset)){
+        if(localStorage.getItem("imagesStored") !== undefined && localStorage.getItem("imagesStored")){
             fetchImages();
         }else{
-            null;
+            localStorage.setItem("imagesStored", "false");
+            fetchImages();
+        }
+    }else{
+        localStorage.setItem("imagesStored", "false");
+        checkScroll();
+    }
+
+    function checkScroll(){
+        console.log("checking scroll")
+        window.onscroll = (ev)=>{
+            /* Checks to see if the gallery is in view by comparing it's position
+            with the height and YOffset of the window.  */
+            if(slothDiv.offsetTop - window.innerHeight < window.pageYOffset && localStorage.getItem("imagesStored") == "false"){
+                console.log("can now fetch images")
+                fetchImages();
+            }else{
+                null;
+            }
         }
     }
 
+
     function fetchImages(){
-        if(sessionStorage.imageIsLoaded !== undefined){
-            let slothImage = sessionStorage.getItem("sloth-image");
-            let mountainsImage = sessionStorage.getItem("mountains-image");
-            let jungleImage = sessionStorage.getItem("jungle-image");
-            let desertsImage = sessionStorage.getItem("deserts-image");
+        if(localStorage.getItem("imagesStored") === "true"){
+            console.log("rendering images")
+            let slothImage = localStorage.getItem("sloth-image");
+            let mountainsImage = localStorage.getItem("mountains-image");
+            let jungleImage = localStorage.getItem("jungle-image");
+            let desertsImage = localStorage.getItem("deserts-image");
 
             slothDiv.style.backgroundImage = `url(${slothImage})`;
             mountainsDiv.style.backgroundImage = `url(${mountainsImage})`;
@@ -43,12 +56,11 @@ window.onload=()=>{
             console.log('fetching image')
             fetch("images/", myInit)
             .then(res=>{
-                sessionStorage.setItem("sloth-image", `${res.url}sloth.jpg`)
-                sessionStorage.setItem("mountains-image", `${res.url}mountains.jpg`)
-                sessionStorage.setItem("jungle-image", `${res.url}jaguar.jpg`)
-                sessionStorage.setItem("deserts-image", `${res.url}fox.png`)
-                sessionStorage.setItem("imageIsLoaded", true)
-
+                localStorage.setItem("sloth-image", `${res.url}sloth.jpg`)
+                localStorage.setItem("mountains-image", `${res.url}mountains.jpg`)
+                localStorage.setItem("jungle-image", `${res.url}jaguar.jpg`)
+                localStorage.setItem("deserts-image", `${res.url}fox.png`)
+                localStorage.setItem("imagesStored", "true")
                 fetchImages();
             })
             .catch(err=>{
@@ -56,5 +68,4 @@ window.onload=()=>{
             })
         }
     }
-
 }
